128f696b90a389a1c5c29c687fcce3d206f96643
Misael Zeferino <misael.zeferino@njpartners.com.br>
2022-03-05 20:52:07 -0300

    Add models; add lunar console; fix pet rotation

--- Assets/Scripts/ARCursor.cs
+++ Assets/Scripts/ARCursor.cs
@@ -11,11 +11,15 @@ public class ARCursor : MonoBehaviour
     public ARPlaneManager m_ARPlaneManager;
     public bool useCursor = true;
 
+    private GameObject InstantiatedObj;
+    private Transform aRSession;
+    private Quaternion objRotation;
+
     void Start()
     {
         cursorChildObject.SetActive(useCursor);
+        aRSession = GameObject.Find("AR Session Origin").transform.GetChild(0);
     }
-
     void Update()
     {
         if (useCursor)
@@ -27,7 +31,15 @@ public class ARCursor : MonoBehaviour
         {
             if (useCursor)
             {
-                GameObject.Instantiate(objectToPlace, transform.position, transform.rotation);
+                if (InstantiatedObj != null)
+                    GameObject.Destroy(InstantiatedObj);
+
+                InstantiatedObj = GameObject.Instantiate(objectToPlace, transform.position, transform.rotation);
+
+                InstantiatedObj.transform.LookAt(aRSession, Vector3.up);
+
+                InstantiatedObj.transform.rotation = new Quaternion(0f, InstantiatedObj.transform.rotation.y, 0f, InstantiatedObj.transform.rotation.w);
+                
                 SetAllPlanesActive(false);
             }
             else
@@ -36,7 +48,9 @@ public class ARCursor : MonoBehaviour
                 raycastManager.Raycast(Input.GetTouch(0).position, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
                 if (hits.Count > 0)
                 {
-                    GameObject.Instantiate(objectToPlace, hits[0].pose.position, hits[0].pose.rotation);
+                    if (InstantiatedObj != null)
+                        GameObject.Destroy(InstantiatedObj);
+                    InstantiatedObj = GameObject.Instantiate(objectToPlace, hits[0].pose.position, hits[0].pose.rotation);
                     SetAllPlanesActive(false);
                 }
             }
@@ -48,6 +62,7 @@ public class ARCursor : MonoBehaviour
         Vector2 screenPosition = Camera.main.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
         List<ARRaycastHit> hits = new List<ARRaycastHit>();
         raycastManager.Raycast(screenPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
 
         if (hits.Count > 0)
         {
